55369693989be947542aa7316ac99d42125762d5
Aleksandr <vzlomed@gmail.com>
2021-07-16 13:23:32 +0300

    Change port x2

--- build/server.js
+++ build/server.js
@@ -2,6 +2,4 @@
 const express = require("express");
 const app = express();
 app.use(express.static(__dirname, { dotfiles: "allow" }));
-app.listen(80, () => {
-    console.log("HTTP server running on port 80");
-});
+app.listen(process.env.PORT, () => {});
