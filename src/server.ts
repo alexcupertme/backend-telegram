8291d12fe05eb2b3a06e6ada7665d686be46ca18
Aleksandr <vzlomed@gmail.com>
2021-07-16 13:21:37 +0300

    Change port for SSL verification

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,4 @@ const app = express();
 
 app.use(express.static(__dirname, { dotfiles: "allow" }));
 
-app.listen(80, () => {
-	console.log("HTTP server running on port 80");
-});
+app.listen(process.env.PORT, () => {});
