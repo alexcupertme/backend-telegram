a0f3c2e1dc1d1541875598500e35cb35e9540ce8
Aleksandr <vzlomed@gmail.com>
2021-07-23 23:50:24 +0300

    Change IP declaration in Rate limiter, because while using with Cloudflare, it puts random ip to limiter

--- src/middlewares/rate-limiter.middleware.ts
+++ src/middlewares/rate-limiter.middleware.ts
@@ -5,13 +5,15 @@ import Redis from "@library/redis";
 import { NextFunction, Request, Response } from "express";
 
 export async function rateLimiterMiddleware(request: Request, response: Response, next: NextFunction) {
-	const ip = request.ip;
-	const isActive = await Redis.client.exists(ip);
-	await Redis.client.incr(ip);
-	if (!isActive) {
-		await Redis.client.expire(ip, response.locals.methodEl.timeRequestRate);
+	const ip = request.headers["x-requested-with"];
+	if (ip && !Array.isArray(ip)) {
+		const isActive = await Redis.client.exists(ip);
+		await Redis.client.incr(ip);
+		if (!isActive) {
+			await Redis.client.expire(ip, response.locals.methodEl.timeRequestRate);
+		}
+		const requestCount = await Redis.client.get(ip);
+		if (requestCount! >= response.locals.methodEl.maxRequestRate) next(new HttpException(0, "", ErrorCodes.badRequest.rateLimit));
+		else next();
 	}
-	const requestCount = await Redis.client.get(ip);
-	if (requestCount! >= response.locals.methodEl.maxRequestRate) next(new HttpException(0, "", ErrorCodes.badRequest.rateLimit));
-	else next();
 }
