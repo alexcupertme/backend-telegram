a61c2300aec6d918129f28018c43a1a39c8a8580
Aleksandr <vzlomed@gmail.com>
2021-07-24 00:31:21 +0300

    Add new IP declaration for proxied server (Cloudflare)

--- src/middlewares/rate-limiter.middleware.ts
+++ src/middlewares/rate-limiter.middleware.ts
@@ -5,13 +5,15 @@ import Redis from "@library/redis";
 import { NextFunction, Request, Response } from "express";
 
 export async function rateLimiterMiddleware(request: Request, response: Response, next: NextFunction) {
-	const ip = request.ip;
+	const ip = request.headers["x-forwarded-for"] && !Array.isArray(request.headers["x-forwarded-for"]) ? request.headers["x-forwarded-for"] : request.ip;
 	const isActive = await Redis.client.exists(ip);
 	await Redis.client.incr(ip);
 	if (!isActive) {
 		await Redis.client.expire(ip, response.locals.methodEl.timeRequestRate);
 	}
 	const requestCount = await Redis.client.get(ip);
-	if (requestCount! >= response.locals.methodEl.maxRequestRate) next(new HttpException(0, "", ErrorCodes.badRequest.rateLimit));
-	else next();
+	if (requestCount! >= response.locals.methodEl.maxRequestRate) {
+		await Redis.client.expire(ip, response.locals.methodEl.timeRequestRate);
+		next(new HttpException(0, "", ErrorCodes.badRequest.rateLimit));
+	} else next();
 }
